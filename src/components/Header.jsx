f12df978155e9423072969391aa0c808b15931d9
dennismuturi <dennismuturi1@gmail.com>
2023-01-11 13:44:47 +0300

    added background image for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,24 +8,24 @@ export default function Header(){
 <div className="container mx-auto px-6 ">
        
 
-<div class="w-full max-w-xl">
-  <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 h-80 p-36">
-    <div class="mb-4">
-      <label class="block text-gray-700 font-bold mb-2 text-2xl" for="customer">
+<div className="w-full max-w-xl h-max">
+  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 h-max p-36">
+    <div className="mb-4">
+      <label class="block text-gray-700 font-bold mb-2 text-3xl py-12" for="customer">
         Request a Mover
       </label>
-      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter PickUp Location"/>
+      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter PickUp Location"/>
     </div>
-    <div class="mb-6">
-      <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter Destination Location"/>
+    <div className="mb-6 ">
+      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="Enter Destination Location"/>
     </div>
-    <div class="flex items-center justify-between">
-      <button class="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+    <div className="flex items-center justify-between">
+      <button className="bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
         Request Now
       </button>
     </div>
   </form>
-  <p class="text-center text-gray-500 text-xs">
+  <p className="text-center text-gray-500 text-xs">
     &copy;2020 Acme Corp. All rights reserved.
   </p>
 </div>
