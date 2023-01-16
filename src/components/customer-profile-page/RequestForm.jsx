d04a9fc92f6ddc65d682efd996b38f5e8a303275
Francis Maina <sebastianfrancix@gmail.com>
2023-01-17 02:53:42 +0300

    added styling to the request mover form

--- src/components/customer-profile-page/RequestForm.jsx
+++ src/components/customer-profile-page/RequestForm.jsx
@@ -5,23 +5,26 @@ const RequestForm = () => {
   return (
     <div className="form_container">
       <div className="tagline">
-        <h2 className="heading">Where can we relocate you from?</h2>
+        <h2 className="header">Where can we relocate you from?</h2>
       </div>
       <form action="submit" className="request_form">
-        <input type="text" placeholder="Add pickup location" />
-        <input type="text" placeholder="Add final destination" />
+        <input type="text" placeholder="Add a pickup location" />
+        <input type="text" placeholder="Enter your destination" />
         <button type="submit" className="request_mover_btn">
-          <i class="uil uil-truck"></i> Request Mover
+          <i class="uil uil-truck truck_icon"></i> Request Mover
         </button>
       </form>
+
       <div className="location_access">
-        <i class="uil uil-location-point"></i>
         <div className="location_access_content">
+          <i class="uil uil-location-point location_icon"></i>
           <h3 className="heading">Allow location access</h3>
-          <p>For perfect pickup experience</p>
+
+          {/* <span className="sub_heading">For perfect pickup experience</span> */}
         </div>
+
         <div className="set_location">
-          <i class="uil uil-map-pin-alt"></i>
+          <i class="uil uil-map-pin-alt location_icon"></i>
           <h3 className="heading">Set Location on map</h3>
         </div>
       </div>
