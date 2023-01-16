703943ec3ee5e016a9f7dcc3196c11ed53649015
Francis Maina <sebastianfrancix@gmail.com>
2023-01-16 23:16:21 +0300

    added the skeleton of the customer page navbar component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import { HomePage } from "./components";
-import NavBar from "./components/NavBar";
-import { Routes, Route} from 'react-router-dom';
+// import { HomePage } from "./components";
+// import NavBar from "./components/NavBar";
+
+import CustomerProfilePage from "./components/customer-profile-page/CustomerProfilePage";
+
+// import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-  <NavBar/>
+      {/* <NavBar/>
   <Routes>
   <Route path="/" element={<HomePage/>}/>
-  </Routes>
-   </>
+  </Routes> */}
+      <CustomerProfilePage />
+    </>
   );
 }
 
---
+++ src/components/customer-profile-page/CustomerProfilePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const CustomerProfilePage = () => {
+  return (
+    <>
+      <Navbar />
+      <h2>Customer Profile Page Component</h2>
+    </>
+  );
+};
+
+export default CustomerProfilePage;
