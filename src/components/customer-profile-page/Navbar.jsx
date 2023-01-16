4e20167d2487101daa126fc975c0f9fb8b4029db
Francis Maina <sebastianfrancix@gmail.com>
2023-01-17 00:00:07 +0300

    added the nav-toggle funtionality

--- src/components/customer-profile-page/Navbar.jsx
+++ src/components/customer-profile-page/Navbar.jsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./navbar.css";
 
 const Navbar = () => {
+  const [active, setActive] = useState("nav_menu");
+  const [toggleIcon, setToggleIcon] = useState("nav_toggler");
+
+  // Nav Toggle Functionality
+  const navToggle = () => {
+    active === "nav_menu"
+      ? setActive("nav_menu nav_active")
+      : setActive("nav_menu");
+  };
+
   return (
     <nav className="nav">
       <a href="#" className="logo">
         Movers App
       </a>
-      <ul className="nav_menu">
+      <ul className={active}>
         <li className="nav_item">
           <a href="#" className="nav_link">
             Home
           </a>
+        </li>
+
+        <li className="nav_item">
           <a href="#" className="nav_link">
             Relocate Me
           </a>
+        </li>
+
+        <li className="nav_item">
           <a href="#" className="nav_link">
             FAQs
           </a>
+        </li>
+        <li className="nav_item">
           <a href="#" className="nav_link">
             My Profile
           </a>
@@ -25,6 +43,11 @@ const Navbar = () => {
       </ul>
 
       {/* Nav Toggler #Hamburger */}
+      <div onClick={navToggle} className={toggleIcon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
     </nav>
   );
 };
