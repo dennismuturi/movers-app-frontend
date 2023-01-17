fcda23c7d84cc2cda7478f5edd20159840bcca35
ChepkemoiMaurene <baurene.chepkemoi@student.moringaschool.com>
2023-01-17 22:36:09 +0300

    added signup component and its functionality

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import MoverPage from "./components/MoverPage";
 import Vehicle from "./components/Vehicle";
 import MoverSetting from "./components/MoverSetting";
 import MoverCustomers from "./components/MoverCustomers";
+import SignupForm from "./components/Signup";
+import LoginForm from "./components/LoginPage";
 
 function App() {
   return (
     <>
   <Routes>
   <Route path="/" element={<HomePage/>}/>
+  <Route path="/signup" element={<SignupForm/>}/>
+    <Route path="/login" element={<LoginForm/>}/>
+
   <Route path="/mover/:id" element={<MoverPage/>}>
     <Route path="vehicle" element={<Vehicle />}/>
     <Route path="settings" element={<MoverSetting/>} />
      <Route path="customers" element={<MoverCustomers />} />
   </Route>
+
   </Routes>
    </>
   );
