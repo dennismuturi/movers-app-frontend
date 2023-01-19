d62c20599e6b7450acdfa48a2209f165db3677e6
Francis Maina <sebastianfrancix@gmail.com>
2023-01-19 06:59:05 +0300

    re-positioned the components and styled them to be responsive on larger devices.

--- src/components/customer-profile-page/CustomerProfilePage.jsx
+++ src/components/customer-profile-page/CustomerProfilePage.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Navbar from "./Navbar";
 import RequestForm from "./RequestForm";
 import Map from "./Map";
-
+import "./styles/customerprofile.css";
 const CustomerProfilePage = () => {
   return (
     <>
-      <Navbar />
-      <Map />
-      <RequestForm />
+      <div className="nav_profile">
+        <Navbar />
+        <div className="content_container">
+          <div className="map_profile">
+            <Map />
+          </div>
+          <div className="request_form_profile">
+            <RequestForm />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
